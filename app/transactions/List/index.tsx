1b8f2bd55f403f5d715c9859dfdd627044256b09
Rahul Jindal <rahul.jindaliit@gmail.com>
2024-08-24 23:31:09 +0530

    chore: show transaction

--- app/transactions/List/index.tsx
+++ app/transactions/List/index.tsx
@@ -1,33 +1,42 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { Typography, Box } from "@mui/material";
+import {
+  Box,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  TableContainer,
+  Paper,
+  Tooltip,
+} from "@mui/material";
 import { Transaction } from "@/types/transaction";
 import { truncateString } from "@/lib/utils";
 import Pagination from "@/components/Pagination";
 import { listTransactions } from "@/services/transaction.action";
+import dayjs from "dayjs";
 
 export default function TransactionList() {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
   const [totalCount, setTotalCount] = useState<number>(0);
-
   const [currentPage, setCurrentPage] = useState(1);
 
   const EmptyTransactionsMessage = () => {
     if (transactions.length > 0) {
-      return <></>;
+      return null;
     }
 
     return (
       <Box component={"p"}>
-        Oops! No transactions found. Lets trying adding one.
+        Oops! No transactions found. Let's try adding one.
       </Box>
     );
   };
 
   const fetchTransaction = async () => {
     const { transactions, totalCount } = await listTransactions();
-    console.log(transactions);
     setTransactions(transactions);
     setTotalCount(totalCount);
   };
@@ -39,35 +48,47 @@ export default function TransactionList() {
   return (
     <div className="space-y-4 flex flex-col justify-center items-center w-[100%]">
       <EmptyTransactionsMessage />
-      <div className="w-[100%] h-[100%]">
-        {transactions.map((transaction) => (
-          <div
-            key={transaction.id}
-            className="flex justify-between items-center p-4 border rounded-md shadow-sm "
-          >
-            <div className="flex flex-col">
-              <Typography variant="subtitle2" className="text-gray-500">
-                {new Date(transaction.transactionDate).toLocaleDateString()}
-              </Typography>
-              <Typography variant="h6" className="font-medium">
-                {truncateString(transaction.description, 20)}
-              </Typography>
-            </div>
-            <Typography
-              variant="h6"
-              className={`font-bold ${
-                transaction.transactionType === "Expense"
-                  ? "text-red-500"
-                  : "text-green-500"
-              }`}
-            >
-              {transaction.transactionType === "Expense"
-                ? `- ₹${transaction.amount}`
-                : `+ ₹${transaction.amount}`}
-            </Typography>
-          </div>
-        ))}
-      </div>
+      <TableContainer component={Paper} className="w-[100%] h-[100%]">
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Date</TableCell>
+              <TableCell>Description</TableCell>
+              <TableCell align="right">Amount</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {transactions.map((transaction) => (
+              <TableRow key={transaction.id}>
+                <TableCell className="w-[20%]">
+                  {dayjs(transaction.transactionDate).format(
+                    "D MMM YYYY, h:mm A"
+                  )}
+                </TableCell>
+                <TableCell className="w-[80%]">
+                  <Tooltip title={transaction.description}>
+                    <Box component={"p"}>
+                      {truncateString(transaction.description, 60)}
+                    </Box>
+                  </Tooltip>
+                </TableCell>
+                <TableCell
+                  align="right"
+                  className={`font-bold w-[20%] ${
+                    transaction.transactionType === "Expense"
+                      ? "text-red-500"
+                      : "text-green-500"
+                  }`}
+                >
+                  {transaction.transactionType === "Expense"
+                    ? `- ₹${transaction.amount}`
+                    : `+ ₹${transaction.amount}`}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
       <Pagination
         currentPage={currentPage}
         totalCount={Number(totalCount)}
