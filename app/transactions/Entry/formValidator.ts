8c72978728b0e581b23d6e537a244427cda3e3ba
Rahul Jindal <rahul.jindaliit@gmail.com>
2024-08-25 13:06:50 +0530

    chore: move form validatior out of component

---
+++ app/transactions/Entry/formValidator.ts
@@ -0,0 +1,16 @@
+import * as yup from "yup";
+
+import { TransactionType } from "@/enums/TransactionType";
+
+export const validationSchema = yup.object().shape({
+  description: yup.string().required("Description is required"),
+  amount: yup
+    .number()
+    .positive("Amount must be greater than 0")
+    .required("Amount is required"),
+  transactionType: yup
+    .string()
+    .oneOf(Object.values(TransactionType))
+    .required("Transaction Type is required"),
+  transactionDate: yup.date().required("Transaction Date is required"),
+});
