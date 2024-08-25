278845ff91200783d2d50bb3bbdd0a431b76b1f1
Rahul Jindal <rahul.jindaliit@gmail.com>
2024-08-25 13:41:50 +0530

    chore: Add categories

---
+++ constants/Categories.tsx
@@ -0,0 +1,83 @@
+import AccessibilityNewIcon from "@mui/icons-material/AccessibilityNew";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import BrushIcon from "@mui/icons-material/Brush";
+import BusinessIcon from "@mui/icons-material/Business";
+import CableIcon from "@mui/icons-material/Cable";
+import CakeIcon from "@mui/icons-material/Cake";
+import CardGiftcardIcon from "@mui/icons-material/CardGiftcard";
+import CelebrationIcon from "@mui/icons-material/Celebration";
+import ChildCareIcon from "@mui/icons-material/ChildCare";
+import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
+import FastfoodIcon from "@mui/icons-material/Fastfood";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import FlashOnIcon from "@mui/icons-material/FlashOn";
+import FlightIcon from "@mui/icons-material/Flight";
+import FreeBreakfastIcon from "@mui/icons-material/FreeBreakfast";
+import GavelIcon from "@mui/icons-material/Gavel";
+import GroupIcon from "@mui/icons-material/Group";
+import HandymanIcon from "@mui/icons-material/Handyman";
+import HomeIcon from "@mui/icons-material/Home";
+import ImportContactsIcon from "@mui/icons-material/ImportContacts";
+import LibraryBooksIcon from "@mui/icons-material/LibraryBooks";
+import LocalActivityIcon from "@mui/icons-material/LocalActivity";
+import LocalBarIcon from "@mui/icons-material/LocalBar";
+import LocalGroceryStoreIcon from "@mui/icons-material/LocalGroceryStore";
+import LocalHospitalIcon from "@mui/icons-material/LocalHospital";
+import LocalPostOfficeIcon from "@mui/icons-material/LocalPostOffice";
+import MovieIcon from "@mui/icons-material/Movie";
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import PaidIcon from "@mui/icons-material/Paid";
+import PetsIcon from "@mui/icons-material/Pets";
+import RestaurantIcon from "@mui/icons-material/Restaurant";
+import RestaurantMenuIcon from "@mui/icons-material/RestaurantMenu";
+import SavingsIcon from "@mui/icons-material/Savings";
+import SchoolIcon from "@mui/icons-material/School";
+import SecurityIcon from "@mui/icons-material/Security";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import SpaIcon from "@mui/icons-material/Spa";
+import StoreIcon from "@mui/icons-material/Store";
+import TravelExploreIcon from "@mui/icons-material/TravelExplore";
+import WorkIcon from "@mui/icons-material/Work";
+
+const categories = [
+  { id: 1, name: "Food & Dining", icon: <RestaurantIcon /> },
+  { id: 2, name: "Transportation", icon: <DirectionsCarIcon /> },
+  { id: 3, name: "Utilities", icon: <FlashOnIcon /> },
+  { id: 4, name: "Housing", icon: <HomeIcon /> },
+  { id: 5, name: "Healthcare", icon: <LocalHospitalIcon /> },
+  { id: 6, name: "Insurance", icon: <SecurityIcon /> },
+  { id: 7, name: "Entertainment", icon: <MovieIcon /> },
+  { id: 8, name: "Shopping", icon: <ShoppingCartIcon /> },
+  { id: 9, name: "Education", icon: <SchoolIcon /> },
+  { id: 10, name: "Personal Care", icon: <SpaIcon /> },
+  { id: 11, name: "Savings", icon: <SavingsIcon /> },
+  { id: 12, name: "Gifts & Donations", icon: <CardGiftcardIcon /> },
+  { id: 13, name: "Travel", icon: <TravelExploreIcon /> },
+  { id: 14, name: "Electronics", icon: <CableIcon /> },
+  { id: 15, name: "Fitness", icon: <FitnessCenterIcon /> },
+  { id: 16, name: "Pets", icon: <PetsIcon /> },
+  { id: 17, name: "Music", icon: <MusicNoteIcon /> },
+  { id: 18, name: "Business", icon: <BusinessIcon /> },
+  { id: 19, name: "Childcare", icon: <ChildCareIcon /> },
+  { id: 20, name: "Groceries", icon: <LocalGroceryStoreIcon /> },
+  { id: 21, name: "Work", icon: <WorkIcon /> },
+  { id: 22, name: "Beauty", icon: <BrushIcon /> },
+  { id: 23, name: "Taxes", icon: <GavelIcon /> },
+  { id: 24, name: "Dining Out", icon: <RestaurantMenuIcon /> },
+  { id: 25, name: "Payments", icon: <PaidIcon /> },
+  { id: 26, name: "Breakfast", icon: <FreeBreakfastIcon /> },
+  { id: 27, name: "Fast Food", icon: <FastfoodIcon /> },
+  { id: 28, name: "Postage", icon: <LocalPostOfficeIcon /> },
+  { id: 29, name: "Events", icon: <LocalActivityIcon /> },
+  { id: 30, name: "Books", icon: <LibraryBooksIcon /> },
+  { id: 31, name: "Reading", icon: <ImportContactsIcon /> },
+  { id: 32, name: "Repair", icon: <HandymanIcon /> },
+  { id: 33, name: "Finance", icon: <AttachMoneyIcon /> },
+  { id: 34, name: "Groups", icon: <GroupIcon /> },
+  { id: 35, name: "Bar", icon: <LocalBarIcon /> },
+  { id: 36, name: "Store", icon: <StoreIcon /> },
+  { id: 37, name: "Birthdays", icon: <CakeIcon /> },
+  { id: 38, name: "Accessibility", icon: <AccessibilityNewIcon /> },
+  { id: 39, name: "Celebrations", icon: <CelebrationIcon /> },
+  { id: 40, name: "Flights", icon: <FlightIcon /> },
+];
